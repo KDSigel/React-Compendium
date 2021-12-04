a56dc5d73a1c29b149388d7543df2029e35729b9
KDSigel <72047979+KDSigel@users.noreply.github.com>
2021-12-03 16:49:33 -0800

    Revert "Dev"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
-import Compendium from './Views/Compendium';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Compendium />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import { screen, render, getByText } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-it('testing the App will render', () => {
-render(<App />)
-const isLoading = screen.getByText('No Poke for you!...')
-expect(isLoading).toBeInTheDocument()
-}
-)
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
